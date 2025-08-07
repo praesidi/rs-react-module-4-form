c7cb0c4a6c135ac57b2ca76c7c8636b2ae07efa5
Elmir Khalilov <elmirkhalilov.dev@gmail.com>
2025-08-07 23:04:29 +0300

    feat: make custom input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,10 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, type ChangeEvent, type ReactNode } from "react";
 import styles from "./input.module.css";
 import Icon from "../icon/icon";
 
-type Size = "xs" | "sm" | "md" | "lg" | "xl";
-
+type Size = "small" | "default" | "large";
+type Radius = "xs" | "sm" | "md" | "lg" | "xl";
 type Variant = "outlined" | "filled" | "borderless" | "underlined";
 
 interface Props {
@@ -17,18 +16,18 @@ interface Props {
   description?: string;
   error?: string;
   variant?: Variant;
-  radius?: Size;
+  radius?: Radius;
   size?: Size;
   disabled?: boolean;
   with_asterisk?: boolean;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => unknown;
   icon?: ReactNode;
   className?: string;
+  style?: React.CSSProperties;
 }
 
 export const Input = ({
   type = "text",
-  id,
   name,
   placeholder,
   label,
@@ -37,15 +36,15 @@ export const Input = ({
   error,
   variant = "outlined",
   radius = "md",
-  size = "md",
+  size = "default",
   disabled,
   with_asterisk,
   onChange,
   icon,
   className = "",
+  style
 }: Props) => {
   const [inputType, setInputType] = useState(type);
-  const inputID = id ? id : `input-${type}`;
 
   const togglePasswordDisplay = () => {
     if (inputType === "password") {
@@ -55,51 +54,44 @@ export const Input = ({
     }
   };
 
+  const iconPaddingCss = icon ? styles.icon_padding : '';
+  const inputErrorCss = error ? styles.error : '';
+  const borderRadiusCss = variant !== 'underlined' ? styles[`bd_radius_${radius}`] : '';
+  const inputCssClasses = `${styles.input} ${iconPaddingCss} ${styles[variant]} ${borderRadiusCss} ${inputErrorCss}`;
+
   return (
     <div
       className={`${styles.root} ${error != "" && styles.error}`}
       data-size={size}
     >
       {/* Input label and description */}
-      {label ? (
-        <label
-          className={styles.label}
-          htmlFor={inputID}
-        >
-          {label}
-          <span className={styles.asterisk}>{with_asterisk ? " *" : ""}</span>
-        </label>
-      ) : null}
-      <p className={styles.description}>{description}</p>
+      { 
+        label 
+          ? <Label
+              text={label}
+              with_asterisk={with_asterisk}
+            /> 
+          : null
+      }
+      { 
+        description
+          ? <p className={styles.description}>{description}</p>
+          : null
+      }
 
       {/* Input and its wrapper */}
-      <div className={`${styles.input_wrapper} ${className}`}>
-        {icon ? <div className={styles.icon_wrapper}>{icon}</div> : null}
+      <div className={`${styles.input_wrapper} ${className}`} style={style}>
+        {icon ? <InputIcon icon={icon}/> : null}
         <input
           type={inputType}
-          id={inputID}
           name={name}
           value={value}
-          className={styles.input}
+          className={inputCssClasses}
           placeholder={placeholder}
           disabled={disabled}
           onChange={onChange}
-          data-variant={variant}
-          data-radius={radius}
         />
-        {type === "password" ? (
-          <div className={styles.show_password_button_wrapper}>
-            <button
-              className={styles.show_password_button}
-              onClick={togglePasswordDisplay}
-            >
-              <Icon
-                name={inputType === "password" ? "eye" : "eye-off"}
-                size="18px"
-              />
-            </button>
-          </div>
-        ) : null}
+        {type === "password" ? <ShowPasswordButton toggleDisplay={togglePasswordDisplay} inputType={inputType}/> : null}
       </div>
 
       {/* Error */}
@@ -107,5 +99,57 @@ export const Input = ({
     </div>
   );
 };
+
+interface LabelProps {
+  text: string;
+  with_asterisk?: boolean;
+}
+
+const Label = ({ text, with_asterisk = false }: LabelProps) => {
+  return (
+      <div
+        className={styles.label}
+      >
+        {text}
+        <span className={styles.asterisk}>{with_asterisk ? " *" : ""}</span>
+      </div>
+  )
+}
+
+interface ShowPasswordButtonProps {
+  inputType: React.HTMLInputTypeAttribute;
+  toggleDisplay: () => void
+}
+
+const ShowPasswordButton = ({ toggleDisplay, inputType }: ShowPasswordButtonProps) => {
+  return (
+    <div className={styles.show_password_button_wrapper}>
+      <button
+        type="button"
+        className={styles.show_password_button}
+        onClick={toggleDisplay}
+      >
+        <Icon
+          name={inputType === "password" ? "eye" : "eye-off"}
+          size="18px"
+        />
+      </button>
+    </div>
+  );
+}
+
+interface InputIconProps {
+  icon: ReactNode;
+}
+
+const InputIcon = ({ icon }: InputIconProps) => {
+  return (
+    <div className={styles.icon_container}>
+      <div className={styles.icon_wrapper}>
+        {icon}
+      </div>
+    </div>
+  );
+}
 
 export default Input;
